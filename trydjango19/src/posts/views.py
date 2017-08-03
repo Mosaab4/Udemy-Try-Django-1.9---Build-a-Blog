9aa4408bf341cedcde951d28f6db1c9d4ee9a71a
mosaab <musab.ehab@gmail.com>
2017-08-03 13:56:39 +0200

    add update view

--- trydjango19/src/posts/views.py
+++ trydjango19/src/posts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse , HttpResponseRedirect
 from django.shortcuts import render , get_object_or_404
 
 from .forms import PostForm
@@ -12,15 +12,9 @@ def post_create(request):
 
 	if form.is_valid():
 		instance = form.save(commit = False)
-		print form.cleaned_data.get("title")
-		print form.cleaned_data.get("content")
 		instance.save()
 
-	# if request.method == "POST":
-	# 	content = request.POST.get("content")
-	# 	title = request.POST.get("title")
-
-	# 	Post.objects.create(title=title,content=content)
+		return HttpResponseRedirect(instance.get_absolute_url())
 	context = {
 		"form": form,
 
@@ -31,7 +25,7 @@ def post_detail(request , id): #read
 	instance = get_object_or_404(Post,id=id)
 
 	context = {
-		"title":"Detail",
+		"title":instance.title,
 		"instance": instance,
 	}
 	return render(request, "post_detail.html", context)
@@ -45,8 +39,25 @@ def post_list(request):
 
 	return render(request, "index.html", context)
 
-def post_update(request):
-	return HttpResponse("<h1>Update</h1>")
+def post_update(request , id=None):
+	instance = get_object_or_404(Post, id = id)
+	form = PostForm(request.POST or None ,instance=instance)
+
+
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+
+		return HttpResponseRedirect(instance.get_absolute_url())
+
+
+	context = {
+		"title":instance.title,
+		"instance": instance,
+		"form":form,
+	}
+
+	return render(request,"post_form.html", context)
 
 def post_delete(request):
 	return HttpResponse("<h1>Delete</h1>")
