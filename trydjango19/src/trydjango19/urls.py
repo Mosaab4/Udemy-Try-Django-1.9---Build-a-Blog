5eb2467ebf69a431770f12273bbbb89ccb0bb803
mosaab <musab.ehab@gmail.com>
2017-08-03 16:31:39 +0200

    Setup Static Files - CSS & Javascript & Images

--- trydjango19/src/trydjango19/urls.py
+++ trydjango19/src/trydjango19/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import url , include
+from django.conf.urls.static import static
 from django.contrib import admin
 from posts import views 
 
@@ -6,3 +8,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^posts/', include("posts.urls" , namespace='posts'))
 ]
+
+
+if settings.DEBUG:
+	urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
