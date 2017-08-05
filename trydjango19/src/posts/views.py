b43b74b63cbb642b5f0917797cd280815effb6d1
mosaab <musab.ehab@gmail.com>
2017-08-05 17:14:48 +0200

    implement post searching

--- trydjango19/src/posts/views.py
+++ trydjango19/src/posts/views.py
@@ -1,6 +1,7 @@
 from urllib.parse import quote_plus
 from django.contrib import messages
 from django.http import HttpResponse , HttpResponseRedirect ,Http404
+from django.db.models import Q
 from django.shortcuts import render , get_object_or_404 , redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from datetime import timezone
@@ -62,8 +63,16 @@ def post_list(request):
 	if request.user.is_staff or request.user.is_superuser :
 		queryset_list = Post.objects.all()
 
+	query = request.GET.get("q")
+	if query:
+		queryset_list = queryset_list.filter(
+			Q(title__icontains=query)|
+			Q(content__icontains=query)|
+			Q(user__first_name__icontains=query)|
+			Q(user__last_name__icontains=query)
+		).distinct()
 
-	paginator = Paginator(queryset_list, 5) # Show 25 contacts per page
+	paginator = Paginator(queryset_list, 2) # Show 25 contacts per page
 
 	page = request.GET.get('page')
 	try:
