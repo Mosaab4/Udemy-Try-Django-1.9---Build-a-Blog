f1e234e94290648727fa8635f65ae60fac370a4b
mosaab <musab.ehab@gmail.com>
2017-08-06 22:50:59 +0200

    implement Django pagedown

--- advancing the blog/src/posts/forms.py
+++ advancing the blog/src/posts/forms.py
@@ -1,8 +1,13 @@
 from django import forms
+
+from pagedown.widgets import PagedownWidget
+
 from .models import Post
 
 class PostForm(forms.ModelForm):
 	"""docstring for PostForm"""
+	content = forms.CharField(widget=PagedownWidget)
+	publish = forms.DateField(widget=forms.SelectDateWidget)
 	class Meta:
 		model = Post
 		fields = [
