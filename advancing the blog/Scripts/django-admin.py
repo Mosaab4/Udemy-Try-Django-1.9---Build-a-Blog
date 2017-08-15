c24bc7ee8ad28c2b4b81e4e268e7044c9e16aa80
mosaab <musab.ehab@gmail.com>
2017-08-15 15:48:57 +0200

    confugure virtualenv

--- advancing the blog/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!e:\codes\try django 1.9 -   build a blog\advancing the blog\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
